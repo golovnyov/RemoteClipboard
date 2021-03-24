035b58bb966340d0287727a40b9cb463408689bd
Viacheslav Holovnov <viacheslav.volovnov@infopulse.com>
2021-03-24 21:09:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TextCopy;
+
+namespace VH.RemoteClipboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+             Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+
+                    services.InjectClipboard();
+                });
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TextCopy;
+
+namespace VH.RemoteClipboard
+{
+    public class Worker : IHostedService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IClipboard clipboard;
+
+        public Worker(ILogger<Worker> logger, IClipboard clipboard)
+        {
+            _logger = logger;
+            this.clipboard = clipboard;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await clipboard.SetTextAsync("Text to place in clipboard1");
+
+            _logger.LogDebug("Setting value to clipboard {}");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping application.");
+        }
+    }
+}
